d035f5697e0a41465b74382f1272671da3fb210e
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-08-17 12:54:23 +0900

    Update mdm.core.js

--- mdm.core.js
+++ mdm.core.js
@@ -1,5 +1,6 @@
 //history
 //v1 make. first call
+//v2 coded the mdm.def mdm.cmd mdm.man
 ;(function(root){
  let mdm=root.mdm||{}
  
@@ -10,5 +11,43 @@
  mdm.is=mdm.is||{} //.xxx //is check
  mdm.re=mdm.re||{} //.xxx //regex
 
+ root.mdm=mdm;
+})(this);
+
+;(function(root){
+ //coded the mdm.def, mdm.cmd, mdm.man 
+ //
+ let mdm=root.mdm||{}
+ mdm.md.man={}
+ mdm.md.cmds={}
+ //cmd definition
+ //definition. man set the text for command help. 
+ mdm.def=(cmd,fn,man)=>{
+  if(!cmd)return;
+  if(!fn)return;
+  mdm.md.man[cmd]=man||cmd+"//: man command Nothing" //command help
+  mdm.md.cmds[cmd]=fn//all command definitions
+  return mdm;
+ }
+ //cmd done
+ mdm.cmd=(cmd,ary)=>{
+  if(!cmd)return;
+  try{
+   mdm.sd['$00']=mdm.md.cmds[cmd].apply(null,ary||[])
+  }catch(e){
+   //cmd call error
+   mdm.sd['$00']=void 0;
+   let mes='command call error! cmd param address line'
+   console.warning(mes,cmd,ary,mdm.sd['$$$'],mdm.md.n)
+  }
+  return mdm;
+ }
+
+ //command help to return value.
+ mdm.man=(cmd)=>{
+  if(!cmd)return Object.keys(mdm.md.man||{}).map(k=>mdm.md.man[k]).join('\n')
+  if(!mdm.md.man[cmd])return cmd+'//: command not defined!'
+  return mdm.md.man[cmd]
+ }  
  root.mdm=mdm;
 })(this);
