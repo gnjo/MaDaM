619365b09acbe1673c63c4fefdfb0e23ad082f00
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-08-17 11:20:33 +0900

    Create mdm.core.js

---
+++ mdm.core.js
@@ -0,0 +1,14 @@
+//history
+//v1 make. first call
+;(function(root){
+ let mdm=root.mdm||{}
+ 
+ mdm.sd=mdm.sd||{} //.xxx //macro valiable set here. like a savedata.
+ mdm.db=mdm.db||{} //.xxx //macro const data set here. like a database.
+ mdm.md=mdm.md||{} //.xxx //madam system valiable set here. fps, lps, looptick, like a madam-data.
+ mdm.fn=mdm.fn||{} //.xxx //useful functions
+ mdm.is=mdm.is||{} //.xxx //is check
+ mdm.re=mdm.re||{} //.xxx //regex
+
+ root.mdm=mdm;
+})(this);
